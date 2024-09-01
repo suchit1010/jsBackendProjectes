12affdd6bc4ecb8fba369e6be34bbfd48e549bf1
suchit1010 <sonisuchit144@gmail.com>
2024-09-01 13:37:48 +0530

    add JWTauthethication

--- SHORT_URL/routes/url.js
+++ SHORT_URL/routes/url.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {handleGenerateNewShortURL, handlegetAnalitcs } = require("../controllers/url");
+const {handleGenerateNewShortURL, handlegetAnalitcs , handleGetUserURLs } = require("../controllers/url");
 
 const router = express();
 
 router.post('/', handleGenerateNewShortURL);
 
 router.get("/analytics/:shortId", handlegetAnalitcs)
+
 
 module.exports = router;
--- SHORT_URL/service/auth.js
+++ SHORT_URL/service/auth.js
@@ -1,15 +1,26 @@
-const sessionIdTouserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Shre@1010";
 
-function setUser(id, user) {
-    sessionIdTouserMap.set(id, user);
+function setUser(user) {
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+        },
+        secret // Optional: Set token to expire in 1 hour
+    );
 }
 
-function getUser(id) {
-    return sessionIdTouserMap.get(id);  // Corrected to return the user
+function getUser(token) {
+    if (!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        return null; // Return null if token is invalid or expired
+    }
 }
-
 
 module.exports = {
     setUser,
     getUser,
-}
+};
